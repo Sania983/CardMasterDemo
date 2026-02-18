c786ebc568030dc9e892321de6736b8cc5145f76
swee2004 <Reddicherla.SaiAlekhya@cognizant.com>
2026-02-18 18:19:43 +0530

    files

--- src/main/java/com/CardMaster/exception/cpl/GlobalExceptionHandlercpl.java
+++ src/main/java/com/CardMaster/exception/cpl/GlobalExceptionHandlercpl.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandlercpl {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<?> handleNotFound(NotFoundException ex) {
