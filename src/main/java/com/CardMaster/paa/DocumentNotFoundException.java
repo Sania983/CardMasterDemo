93c5aa36dfeeabff35b17efbc07bc410b88a3d47
Harsha-Bukka <Bukka.Harsha@cognizant.com>
2026-02-27 11:45:33 +0530

    added

--- src/main/java/com/CardMaster/paa/DocumentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.CardMaster.paa;
-
-public class DocumentNotFoundException extends RuntimeException {
-    public DocumentNotFoundException(String message) {
-        super(message);
-    }
-}
