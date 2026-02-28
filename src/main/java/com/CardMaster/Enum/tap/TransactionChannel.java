a93a5fbc65db4be0b3606551b27fba0286d477f2
2465927 <SufiyaAbdulrajak.Shaikh@cognizant.com>
2026-02-28 14:01:35 +0530

    added

---
+++ src/main/java/com/CardMaster/Enum/tap/TransactionChannel.java
@@ -0,0 +1,7 @@
+package com.CardMaster.Enum.tap;
+
+public enum Channel {
+    POS,          // Point of Sale terminal
+    ONLINE,       // E-commerce / online payment
+    ATM           // ATM withdrawal
+}
